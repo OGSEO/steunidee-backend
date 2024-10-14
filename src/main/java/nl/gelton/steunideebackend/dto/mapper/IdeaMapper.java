6d36fa61c71202ccf84e9f14b7d6c9573e34cddf
Ollie Gelton <olliegelton@gmail.com>
2024-10-14 12:46:39 +0200

    lets go

--- src/main/java/nl/gelton/steunideebackend/dto/mapper/IdeaMapper.java
+++ src/main/java/nl/gelton/steunideebackend/dto/mapper/IdeaMapper.java
@@ -1,10 +1,7 @@
 package nl.gelton.steunideebackend.dto.mapper;
 
-import nl.gelton.steunideebackend.dto.input.CommentInputDto;
 import nl.gelton.steunideebackend.dto.input.IdeaInputDto;
-import nl.gelton.steunideebackend.dto.output.CommentOutputDto;
 import nl.gelton.steunideebackend.dto.output.IdeaOutputDto;
-import nl.gelton.steunideebackend.model.Comment;
 import nl.gelton.steunideebackend.model.Idea;
 
 public class IdeaMapper {
@@ -21,6 +18,7 @@ public class IdeaMapper {
         ideaOutputDto.setId(idea.getId());
         ideaOutputDto.setTitle(idea.getTitle());
         ideaOutputDto.setDescription(idea.getDescription());
+        ideaOutputDto.setCreatedAt(idea.getCreatedAt());
         return ideaOutputDto;
     }
 }
